280f9328e80dd38d19c1f0145f882f276c3e52c2
Akn <sibata486@gmail.com>
2021-09-03 20:13:31 +0900

    add / ドラッグアンドドロップを行うための Behavior を追加

--- TextUtil/ViewModels/MainWindowViewModel.cs
+++ TextUtil/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using Prism.Mvvm;
+using System.IO;
 
 namespace TextUtil.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private string _title = "Prism Application";
-        public string Title
-        {
+        public string Title {
             get { return _title; }
             set { SetProperty(ref _title, value); }
+        }
+
+        private FileInfo _currentFileInfo;
+        public FileInfo CurrentFileInfo {
+            get { return _currentFileInfo; }
+            set { SetProperty(ref _currentFileInfo, value); }
         }
 
         public MainWindowViewModel()
