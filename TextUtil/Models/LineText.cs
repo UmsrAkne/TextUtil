de8897036e3b723bb325e30550b5a4dfd851987f
Akn <sibata486@gmail.com>
2022-08-07 14:03:14 +0900

    add / テキスト一行を表すクラスを追加

---
+++ TextUtil/Models/LineText.cs
@@ -0,0 +1,17 @@
+﻿namespace TextUtil.Models
+{
+    using Prism.Mvvm;
+
+    public class LineText : BindableBase
+    {
+        private string text;
+        private int index;
+        private int counter;
+
+        public string Text { get => text; set => SetProperty(ref text, value); }
+
+        public int Index { get => index; set => SetProperty(ref index, value); }
+
+        public int Counter { get => counter; set => SetProperty(ref counter, value); }
+    }
+}
