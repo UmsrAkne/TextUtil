e3821e1324c3c488923d545c35f8f9dea8931f06
Akn <sibata486@gmail.com>
2022-08-13 13:25:32 +0900

    add / LineText に Undo() を実装

---
+++ TextUtilTests/Models/LineTextTests.cs
@@ -0,0 +1,31 @@
+﻿namespace TextUtil.Models.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LineTextTests
+    {
+        [TestMethod]
+        public void UndoTest()
+        {
+            var line = new LineText();
+
+            line.Text = "a";
+            line.Text = "b";
+            line.Text = "c";
+            line.Text = "d";
+
+            line.Undo();
+            Assert.AreEqual("c", line.Text);
+
+            line.Undo();
+            Assert.AreEqual("b", line.Text);
+
+            line.Undo();
+            Assert.AreEqual("a", line.Text);
+
+            line.Undo();
+            Assert.AreEqual("a", line.Text);
+        }
+    }
+}
